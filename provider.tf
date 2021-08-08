6d7410d2020a7c5a7e37564e9afd856367951575
Edgardo Quiroga <edgardo.quiroga@gmail.com>
2021-08-08 19:42:21 +0000

    Updating provider to allow EKS cluster create by Terragrunt role

--- provider.tf
+++ provider.tf
@@ -1,13 +1,17 @@
 data "external" "aws_iam_authenticator" {
   program = ["sh", "-c", "aws-iam-authenticator token -i ${aws_eks_cluster.main.name} | jq -r -c .status"]
 }
- 
+
+data "aws_eks_cluster_auth" "main" {
+  name = "${aws_eks_cluster.main.name}"
+}
+
 provider "kubernetes" {
   host                      = aws_eks_cluster.main.endpoint
   cluster_ca_certificate    = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
-  token                     = data.external.aws_iam_authenticator.result.token
+  token                     = data.aws_eks_cluster_auth.main.token
   load_config_file          = false
-  version                   = "~> 1.7"
+  version                   = "~> 1.9"
 }
 
 provider "helm" {
