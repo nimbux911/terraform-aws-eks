6840539ab500c9e6588a99dfa533a9ef32734343
Emanuel Quiroga <emanuel.quiroga@gapac.com>
2022-05-04 07:11:53 -0300

    remove aws-iam-authenticator

--- provider.tf
+++ provider.tf
@@ -1,17 +1,15 @@
-data "external" "aws_iam_authenticator" {
-  program = ["sh", "-c", "aws-iam-authenticator token -i ${aws_eks_cluster.main.name} | jq -r -c .status"]
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = "${aws_eks_cluster.main.name}"
-}
-
+#data "external" "aws_iam_authenticator" {
+#  program = ["sh", "-c", "aws-iam-authenticator token -i ${aws_eks_cluster.main.name} | jq -r -c .status"]
+#}
+ 
 provider "kubernetes" {
   host                      = aws_eks_cluster.main.endpoint
   cluster_ca_certificate    = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
-  token                     = data.aws_eks_cluster_auth.main.token
-  load_config_file          = false
-  version                   = "~> 1.9"
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
 }
 
 provider "helm" {
