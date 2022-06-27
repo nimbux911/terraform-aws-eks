34d5c04289f838146996653d595975285f5715d6
Emanuel Quiroga <emanuel.quiroga@gapac.com>
2022-06-27 18:39:12 -0300

    adding required_versions for tf providers

--- provider.tf
+++ provider.tf
@@ -1,4 +1,25 @@
- 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.0, ~> 4.20"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.11"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.5.1"
+    }
+  }
+}
+
+
+
 provider "kubernetes" {
   host                      = aws_eks_cluster.main.endpoint
   cluster_ca_certificate    = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
@@ -11,8 +32,8 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
+    host                    = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate  = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
